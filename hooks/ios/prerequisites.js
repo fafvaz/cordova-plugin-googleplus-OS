7b7feb28f0ea4e66c153e66db6f10cd2808458d8
Fabio Vaz <fafvaz@gmail.com>
2021-02-08 15:24:03 +0000

    V7

--- hooks/ios/prerequisites.js
+++ hooks/ios/prerequisites.js
@@ -1,15 +1,9 @@
 console.error("Google Sign-In prerequisites");
 
 module.exports = function (context) {
-	var hooksPath;
-	
-	path = require('path');
-	
-	hooksPath = path.resolve(context.opts.projectRoot, "plugins", context.opts.plugin.id, "hooks/ios");
-	console.log(hooksPath);
   var child_process = require('child_process'),
-      deferral = require(path.resolve(hooksPath, "q.js")).defer();
-		
+      deferral = require('q').defer();
+
   var output = child_process.exec('npm install', {cwd: __dirname},
       function (error) {
         if (error !== null) {
